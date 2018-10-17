d540a4bd8885d1881ed5ca451b5e094909b2e3bb
gayanper <gayanper@gmail.com>
2018-10-17 21:07:53 +0530

    update upload script

--- build-option-plugin.site/upload.sh
+++ build-option-plugin.site/upload.sh
@@ -4,6 +4,11 @@ API_KEY=$1
 ARCHIVE=$2
 VERSION=$3
 
-curl -X PUT -T $ARCHIVE -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/eclipse-build-option-plugin/$VERSION/eclipse-build-option-plugin/$VERSION/archive-site.zip?explode=1
-curl -X POST -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/eclipse-build-option-plugin/$VERSION/publish
+curl -X PUT -T $ARCHIVE -u gayanper:$API_KEY \
+        -H "X-Bintray-Package:eclipse-build-option-plugin" \
+        -H "X-Bintray-Version:$VERSION" \
+        -H "X-Bintray-Explode:1" \
+        https://api.bintray.com/content/gayanper/p2/eclipse-build-option-plugin/$VERSION
+
+curl -X POST -u gayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/eclipse-build-option-plugin/$VERSION/publish
 
